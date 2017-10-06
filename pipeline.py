6fbc3b52f7b5bbc00896d3b9a2af15f89bf229fc
Renee MacDonald <rm51@users.noreply.github.com>
2017-10-05 23:26:09 -0700

    changing color space to HLS and LUV

--- pipeline.py
+++ pipeline.py
@@ -15,20 +15,20 @@ def plot_image(img):
 	img_small = cv2.resize(img, (np.int(img.shape[1] / scale), np.int(img.shape[0] / scale)), interpolation=cv2.INTER_NEAREST)
 
 	# Convert subsampled image to desired color space(s)
-	img_small_RGB = cv2.cvtColor(img_small, cv2.COLOR_BGR2RGB)  # OpenCV uses BGR, matplotlib likes RGB
-	img_small_HSV = cv2.cvtColor(img_small, cv2.COLOR_BGR2HSV)
-	img_small_rgb = img_small_RGB / 255.  # scaled to [0, 1], only for plotting
+	img_small_HLS = cv2.cvtColor(img_small, cv2.COLOR_BGR2HLS)  # OpenCV uses BGR, matplotlib likes RGB
+	img_small_LUV = cv2.cvtColor(img_small, cv2.COLOR_BGR2LUV)
+	img_small_hls = img_small_HLS / 255.  # scaled to [0, 1], only for plotting
 
 	# Plot and show
-	plot3d(img_small_RGB, img_small_rgb)
+	plot3d(img_small_HLS, img_small_hls)
 	plt.show()
 
-	plot3d(img_small_HSV, img_small_rgb, axis_labels=list("HSV"))
+	plot3d(img_small_LUV, img_small_hls, axis_labels=list("LUV"))
 	plt.show()
 
 
 def plot3d(pixels, colors_rgb,
-        axis_labels=list("RGB"), axis_limits=((0, 255), (0, 255), (0, 255))):
+        axis_labels=list("HLS"), axis_limits=((0, 255), (0, 255), (0, 255))):
     """Plot pixels in 3D."""
 
     # Create figure and 3D axes
@@ -56,9 +56,10 @@ def plot3d(pixels, colors_rgb,
     return ax  # return Axes3D object for further manipulation
 
 car_array = []
-cars_path='test_images/*.jpg'
+cars_path='vehicle_images/*.png'
 car_images = glob.glob(cars_path)
 for car_image in car_images:
 	img = cv2.imread(car_image)
+	print (car_image)
 	plot_image(img)
 	car_array.append(img)
